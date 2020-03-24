78da959c4d3431028cbc629629bb897abf35888a
Sho <tpkoni1010@gmail.com>
2020-03-24 16:54:46 +0700

    Done 16 Modules

--- DatingApp-SPA/src/app/_guards/prevent-unsaved-changes.guard.ts
+++ DatingApp-SPA/src/app/_guards/prevent-unsaved-changes.guard.ts
@@ -5,8 +5,13 @@ import { MemberEditComponent } from '../members/member-edit/member-edit.componen
 @Injectable()
 export class PreventUnsavedChanges implements CanDeactivate<MemberEditComponent> {
     canDeactivate(component: MemberEditComponent) {
-        if (component.editForm.dirty) {
-            return confirm('Are you sure? You want to continue? Any unsaved changes will be lost');
+        if (component.introduction !== component.user.introduction
+            || component.lookingFor !== component.user.lookingFor
+            || component.interests !== component.user.interests
+            || component.city !== component.user.city
+            || component.country !== component.user.country
+            ) {
+                return confirm('Are you sure? You want to continue? Any unsaved changes will be lost');
         }
         return true;
     }
--- DatingApp-SPA/src/app/members/member-edit/member-edit.component.ts
+++ DatingApp-SPA/src/app/members/member-edit/member-edit.component.ts
@@ -5,6 +5,7 @@ import { AlertifyService } from 'src/app/_services/alertify.service';
 import { NgForm } from '@angular/forms';
 import { UserService } from 'src/app/_services/user.service';
 import { AuthService } from 'src/app/_services/auth.service';
+import { AngularWaitBarrier } from 'blocking-proxy/built/lib/angular_wait_barrier';
 
 @Component({
   selector: 'app-member-edit',
@@ -15,12 +16,19 @@ export class MemberEditComponent implements OnInit {
   @ViewChild('editForm') editForm: NgForm;
   user: User;
   photoUrl: string;
+  introduction: any;
+  lookingFor: any;
+  interests: any;
+  city: any;
+  country: any;
+
   @HostListener('window:beforeunload', ['$event'])
   unloadNotification($event: any) {
-    if (this.editForm.dirty) {
+    if (this.checkchange()) {
       $event.returnValue = true;
     }
   }
+
 
   constructor(private route: ActivatedRoute, private alertify: AlertifyService,
               private userService: UserService, private authService: AuthService) { }
@@ -30,6 +38,11 @@ export class MemberEditComponent implements OnInit {
       this.user = data.user;
     });
     this.authService.currentPhotoUrl.subscribe(photoUrl => this.photoUrl = photoUrl);
+    this.introduction = this.user.introduction;
+    this.lookingFor = this.user.lookingFor;
+    this.interests = this.user.interests;
+    this.city = this.user.city;
+    this.country = this.user.country;
   }
 
   updateUser() {
@@ -39,6 +52,19 @@ export class MemberEditComponent implements OnInit {
     }, error => {
       this.alertify.error(error);
     });
+  }
+
+  checkchange(): boolean {
+    if (this.introduction !== this.user.introduction
+        || this.lookingFor !== this.user.lookingFor
+        || this.interests !== this.user.interests
+        || this.city !== this.user.city
+        || this.country !== this.user.country
+        ) {
+      return true;
+    } else {
+      return false;
+    }
   }
 
   updateMainphoto(photoUrl) {
